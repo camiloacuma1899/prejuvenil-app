1ff4718f81ee7c1d52ac5f57b8fa884c609951f8
Camilo Acuña Marriaga <cacuna@DOMAIN.LOCAL>
2026-06-06 12:23:17 -0500

    fix admin render

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,10 @@ from flask_migrate import Migrate
 
 from flask_login import LoginManager
 
+from werkzeug.security import generate_password_hash
+
+import os
+
 
 db = SQLAlchemy()
 
@@ -14,14 +18,13 @@ migrate = Migrate()
 login_manager = LoginManager()
 
 login_manager.login_view = 'main.login'
+
 login_manager.login_message = 'Debes iniciar sesión'
 
 login_manager.login_message_category = 'warning'
 
+
 def create_app():
-
-    import os
-
 
     app = Flask(__name__)
 
@@ -32,10 +35,10 @@ def create_app():
     if database_url:
 
         database_url = database_url.replace(
-        "postgres://",
-        "postgresql://",
-        1
-    )
+            "postgres://",
+            "postgresql://",
+            1
+        )
 
     app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 
@@ -45,11 +48,13 @@ def create_app():
 
     app.config['WTF_CSRF_ENABLED'] = False
 
+
     db.init_app(app)
 
     migrate.init_app(app, db)
 
     login_manager.init_app(app)
+
 
     from app.models.integrante import Integrante
 
@@ -65,31 +70,29 @@ def create_app():
 
     from app.routes.routes import main
 
+
     app.register_blueprint(main)
+
 
     with app.app_context():
 
         db.create_all()
 
-    from app.models.usuario import Usuario
+        admin = Usuario.query.filter_by(
+            username='admin'
+        ).first()
 
-    from werkzeug.security import generate_password_hash
+        if not admin:
 
-    admin = Usuario.query.filter_by(
-        username='admin'
-    ).first()
+            nuevo_admin = Usuario(
+                username='admin',
+                password=generate_password_hash('1234'),
+                rol='coordinador'
+            )
 
-    if not admin:
+            db.session.add(nuevo_admin)
 
-        nuevo_admin = Usuario(
-            username='admin',
-            password=generate_password_hash('1234'),
-            rol='coordinador'
-        )
-
-        db.session.add(nuevo_admin)
-
-        db.session.commit()
+            db.session.commit()
 
 
     return app
@@ -103,4 +106,3 @@ def load_user(user_id):
     return Usuario.query.get(
         int(user_id)
     )
-    
